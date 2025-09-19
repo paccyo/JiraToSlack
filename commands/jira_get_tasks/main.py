cab5d52c80a7a8a834b1124214949ba8a047fbb3
paccyo <zaq1122s@gmail.com>
2025-09-19 17:13:19 +0900

    add: Added simple llm to jira get tasks

--- commands/jira_get_tasks/main.py
+++ commands/jira_get_tasks/main.py
@@ -1,11 +1,20 @@
-
+import os
+import google.generativeai as genai
 
 class Command_Jira_Get_Tasks_Responce:    
     
     def __init__(self):
-          pass
+        gemini_api_key = os.environ.get("GEMINI_API_KEY")
+        if not gemini_api_key:
+            raise ValueError("FATAL ERROR: Environment variable GEMINI_API_KEY is not set.")
+
+        genai.configure(api_key=gemini_api_key)
+        self.model = genai.GenerativeModel('Gemini 2.5 Flash-Lite')
     
     def execute(self, body):
-          text = body["text"]
-          responce = f"受け取ったドキュメント： {text}"
-          return responce
+        text = body["text"]
+        try:
+            response = self.model.generate_content(text)
+            return response.text
+        except Exception as e:
+            return f"An error occurred: {e}"
