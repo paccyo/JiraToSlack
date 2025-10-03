eabba03e65b1f1b68fe6323d8515e7c11c65dfad
paccyo <zaq1122s@gmail.com>
2025-10-03 09:39:37 +0900

    update: refactaring

--- actions/__init__.py
+++ actions/__init__.py
@@ -1,23 +1,4 @@
-import asyncio
-
-from util.get_jira_data import GetJiraData
-from util.request_jira import RequestJiraRepository
-from util.get_slack_email import GetSlackUserIdToEmail
-
-
-
-def change_status(say, user_id, issue_key, status):
-    """Jira課題を完了ステータスに移動させる関数（バックグラウンドで実行）"""
-    get_skack_user_id_to_email = GetSlackUserIdToEmail()
-    slack_email_to_register = get_skack_user_id_to_email.get_user_email(user_id)
-    
-    get_jira_data = GetJiraData()
-    email = get_jira_data.get_slack_email_to_jira_email(slack_email_to_register)
-
-    request_jira_repository = RequestJiraRepository()
-    request_jira_repository.issue_change_status(email, issue_key, status)
-    
-    return
+import actions.change_status.change_status as change_status
 
 
 def register_actions(app):
@@ -29,23 +10,11 @@ def register_actions(app):
     def handle_move_todo_command(ack, body, say, client):
         ack()
         try:
-            change_status(say, body["user"]["id"], body["actions"][0]["value"], "TODO")
-            say(f"✅ Jira課題 `{body['actions'][0]['value']}` のステータスをTODOに変更しました。")
-            jql_query = f"issue = \"{body['actions'][0]['value']}\""
-            request_jira_repository = RequestJiraRepository()
-            jira_result = request_jira_repository.request_jql(jql_query)[0]
-            block = request_jira_repository.format_jira_issue_for_slack(jira_result)
-            client.chat_postMessage(
-                channel=body["channel"]["id"],
-                text=f"ステータスをTODOに変更",
-                blocks=block
-            )
-
-            
+            change_status(say, client, body, "TODO")
             return
         except Exception as e:
-            print(f"エラーが発生しました: {e}")
-            say(f"エラーが発生しました: {e}")
+            print(f"ステータスの更新中にエラーが発生しました: {e}")
+            say(f"ステータスの更新中にエラーが発生しました: {e}")
             return 
         
 
@@ -53,83 +22,43 @@ def register_actions(app):
     def handle_move_in_progress_command(ack, body, say, client):
         ack()
         try:
-            change_status(say, body["user"]["id"], body["actions"][0]["value"], "IN_progress")
-            say(f"✅ Jira課題 `{body['actions'][0]['value']}` のステータスをIN_progressに変更しました。")
-            jql_query = f"issue = \"{body['actions'][0]['value']}\""
-            request_jira_repository = RequestJiraRepository()
-            jira_result = request_jira_repository.request_jql(jql_query)[0]
-            block = request_jira_repository.format_jira_issue_for_slack(jira_result)
-            client.chat_postMessage(
-                channel=body["channel"]["id"],
-                text=f"ステータスをIN_progressに変更",
-                blocks=block
-            )
-            return 
-        except Exception as e:
-            print(f"エラーが発生しました: {e}")
-            say(f"エラーが発生しました: {e}")
+            change_status(say, client, body, "IN_progress")
             return
+        except Exception as e:
+            print(f"ステータスの更新中にエラーが発生しました: {e}")
+            say(f"ステータスの更新中にエラーが発生しました: {e}")
+            return 
         
     @app.action("move_reviewing")
     def handle_move_in_progress_command(ack, body, say, client):
         ack()
         try:
-            change_status(say, body["user"]["id"], body["actions"][0]["value"], "REVIEWING")
-            say(f"✅ Jira課題 `{body['actions'][0]['value']}` のステータスをREVIEWINGに変更しました。")
-            jql_query = f"issue = \"{body['actions'][0]['value']}\""
-            request_jira_repository = RequestJiraRepository()
-            jira_result = request_jira_repository.request_jql(jql_query)[0]
-            block = request_jira_repository.format_jira_issue_for_slack(jira_result)
-            client.chat_postMessage(
-                channel=body["channel"]["id"],
-                text=f"ステータスをREVIEWINGに変更",
-                blocks=block
-            )
-            return 
-        except Exception as e:
-            print(f"エラーが発生しました: {e}")
-            say(f"エラーが発生しました: {e}")
+            change_status(say, client, body, "REVIEWING")
             return
+        except Exception as e:
+            print(f"ステータスの更新中にエラーが発生しました: {e}")
+            say(f"ステータスの更新中にエラーが発生しました: {e}")
+            return 
 
     @app.action("move_abort")
     def handle_move_abort_command(ack, body, say, client):
         ack()
         try:
-            change_status(say, body["user"]["id"], body["actions"][0]["value"], "Abort")
-            say(f"✅ Jira課題 `{body['actions'][0]['value']}` のステータスをAbortに変更しました。")
-            jql_query = f"issue = \"{body['actions'][0]['value']}\""
-            request_jira_repository = RequestJiraRepository()
-            jira_result = request_jira_repository.request_jql(jql_query)[0]
-            block = request_jira_repository.format_jira_issue_for_slack(jira_result)
-            client.chat_postMessage(
-                channel=body["channel"]["id"],
-                text=f"ステータスをAbortに変更",
-                blocks=block
-            )
+            change_status(say, client, body, "Abort")
             return
         except Exception as e:
-            print(f"エラーが発生しました: {e}")
-            say(f"エラーが発生しました: {e}")
-            return
+            print(f"ステータスの更新中にエラーが発生しました: {e}")
+            say(f"ステータスの更新中にエラーが発生しました: {e}")
+            return 
 
 
     @app.action("move_compleated")
     def handle_move_compleated_command(ack, body, say, client):
         ack()
         try:
-            change_status(say, body["user"]["id"], body["actions"][0]["value"], "完了")
-            say(f"✅ Jira課題 `{body['actions'][0]['value']}` を完了にしました。")
-            jql_query = f"issue = \"{body['actions'][0]['value']}\""
-            request_jira_repository = RequestJiraRepository()
-            jira_result = request_jira_repository.request_jql(jql_query)[0]
-            block = request_jira_repository.format_jira_issue_for_slack(jira_result)
-            client.chat_postMessage(
-                channel=body["channel"]["id"],
-                text=f"ステータスを完了に変更",
-                blocks=block
-            )
+            change_status(say, client, body, "完了")
             return
         except Exception as e:
-            print(f"エラーが発生しました: {e}")
-            say(f"エラーが発生しました: {e}")
+            print(f"ステータスの更新中にエラーが発生しました: {e}")
+            say(f"ステータスの更新中にエラーが発生しました: {e}")
             return
